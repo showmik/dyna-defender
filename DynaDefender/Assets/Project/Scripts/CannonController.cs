c3bc3fcf2f3fc605888f83ece32077c54c26ee90
Showmik Haque <showmik.haque@yahoo.com>
2020-10-27 16:05:34 +0600

    Cannon Controller Implemented

---
+++ DynaDefender/Assets/Project/Scripts/CannonController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class CannonController : MonoBehaviour
+{
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private Transform projectileSpwanPoint;
+    [SerializeField] private float rotationSpeed = 17f;
+    [SerializeField] private float projectileSpeed = 17f;
+    [SerializeField] private float fireDelay = 0.2f;
+
+    private Quaternion targetRotation;
+    private bool hasFired = false;
+    private float fireTimeCounter;
+
+    private void Start()
+    {
+        targetRotation = transform.rotation;
+    }
+
+    private void Update()
+    {
+        TakeInput();
+        RotateCannon();
+        FireProjectile();
+        fireTimeCounter -= Time.deltaTime;
+    }
+
+    private void TakeInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Vector3 touchCordinate = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            if (touchCordinate.y > transform.position.y)
+            {
+                hasFired = true;
+                Vector2 targetVector = touchCordinate - transform.position;
+                targetRotation = Quaternion.FromToRotation(-transform.position, targetVector);
+            }
+        }
+    }
+
+    private void RotateCannon()
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+    }
+
+    private void FireProjectile()
+    {
+        if (transform.rotation == targetRotation && hasFired && fireTimeCounter < 0)
+        {
+            GameObject projectileInstance = Instantiate(projectilePrefab, projectileSpwanPoint.position, transform.rotation);
+            projectileInstance.GetComponent<Projectile>().SetVelocity(17);
+            Destroy(projectileInstance, 2f);
+            hasFired = false;
+            fireTimeCounter = fireDelay;
+        }
+    }
+}
---
+++ DynaDefender/Assets/Project/Scripts/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private float speed = 10f;
+    private Rigidbody2D body2d;
+
+    private void Start()
+    {
+        body2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        body2d.velocity = transform.up * speed;
+    }
+
+    public void SetVelocity(float speed)
+    {
+        this.speed = speed;
+    }
+}
